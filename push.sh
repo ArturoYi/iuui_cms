470d7767ad2b9bf534bc9fb4c08c0030104e8fce
陈柒 <3062995371@qq.com>
2022-08-18 15:08:08 +0800

    2022-08-18

--- push.sh
+++ push.sh
@@ -17,7 +17,7 @@ sudo chmod 755 push.sh
 git init 
 git config --global user.name "陈柒"
 git config --global user.email "3062995371@qq.com"
-git add -A; #添加所有的更新，.只能添加最新的更新
+git add .; #添加所有的更新，.只能添加最新的更新
 git commit -m "${pushmessage}";
 git remote add origin git@gitee.com:chen_qi/iuui-cms.git
 git push -u origin master;
