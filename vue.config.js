110124475306c01463f9b3dab9e8160e4def3e82
陈柒 <3062995371@qq.com>
2022-08-22 11:27:46 +0800

    build

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 
 const path = require("path")
 
-const CompressionWebpackPlugin = require("compression-webpack-plugin")
-const productionGzipExtensions = ["js", "css"]
+// const CompressionWebpackPlugin = require("compression-webpack-plugin")
+// const productionGzipExtensions = ["js", "css"]
 const resolve = (dir) => path.join(__dirname, dir)
 module.exports = defineConfig({
   transpileDependencies: true,
